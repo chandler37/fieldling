35dd915dbbe5cf55ba8892370d9143191fccea72
dchandler <>
2004-07-04 23:08:15 +0000

    This script should help with nightly builds by letting us do the existing build process but then run a script to change times on files.  JWS cares about times on files.
    We still might need to avoid running nightly builds at all unless CVS update
    does something.

---
+++ diffreshen/one_file_diffreshen.sh
@@ -0,0 +1,60 @@
+#! /bin/sh
+
+# diffreshen (restores old modification times for unchanged files).
+# Copyright (C) 2004 David L. Chandler
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+
+# one_file_diffreshen.sh: A subroutine of diffreshen that runs cmp on
+# a single file (two revisions of it, that is) and touches the new
+# revision to have the old revision's modification time unless there
+# is a difference in the two files.
+
+# Usage: $1 is the file, $2 is 'old' path normalized, $3 is 'new' path
+# normalized.
+
+
+# I'm using backslash, not forward slash as a quote character because
+# it's rare for Unix paths to contain a backslash.  If your path does,
+# you're out of luck.  FIXME: Die nicely if that's the case.  FIXME:
+# Which sed versions support sqfooqbarq?
+newfile="$1"
+old="$2"
+new="$3"
+oldfile=`echo "$1" | sed -e "s\\\\${new}\\\\${old}\\\\"`
+if test "$newfile" = "$oldfile"; then
+    echo "Oops!  old file and new file are both '$newfile'"
+    exit 1
+fi
+
+# FIXME: /foo/foo-old/bar and /foo/foo/bar give us trouble if
+# 'diffreshen foo-old foo' is used.  So test for the existence of
+# s/^$new/$old/ and abort if it exists.
+
+# With my version of cmp, this test is redundant because cmp returns 2
+# on trouble and 1 for a diff.  Both are treated the same way.  But
+# for clarity and robustness:
+if test -x "$oldfile"; then
+    if cmp "$oldfile" "$newfile" 1>/dev/null 2>&1; then
+        # FIXME: Find out which versions of touch support this and
+        # document that as a dependence.
+        touch -r "$oldfile" "$newfile"
+        if test "$DFVERBOSE" = "true"; then
+            echo "touching '$newfile' so that is has the same modification time as '$oldfile'"
+        fi
+    fi
+fi
+exit 0
