3a7e0925dba6bf6aa4ff54f82880d3968237c9b8
amontano <>
2005-04-02 05:21:39 +0000

    Added the ability to change the language of the interface and the font to display it from the preference->display option. Now only chinese and english is supported.

--- Fieldling/src/fieldling/util/I18n.java
+++ Fieldling/src/fieldling/util/I18n.java
@@ -1,12 +1,21 @@
 package fieldling.util;
 
+import fieldling.quilldriver.PreferenceManager;
+
+import java.awt.Font;
+import java.awt.GraphicsEnvironment;
 import java.util.Locale;
 import java.util.ResourceBundle;
 import javax.swing.JComponent;
+import javax.swing.UIManager;
+import javax.swing.plaf.FontUIResource;
+
+import org.thdl.util.SimplifiedLinkedList;
 
 public class I18n {
 	private static Locale locale = null;
 	private static ResourceBundle resources = null;
+	private static String[] languageCodes = {"en", "zh"};
 
 	private I18n() {}
 
@@ -17,9 +26,18 @@ public class I18n {
 			
 			// testing chinese:
 			// resources = ResourceBundle.getBundle("MessageBundle", Locale.SIMPLIFIED_CHINESE);
-		}
+		}	
+		
+		/*
+		 * Load the correct font in order to display properly
+		 * the resourceBundle just loaded.
+		 */
+		if (PreferenceManager.default_interface_font!=null)
+			setSystemFont(PreferenceManager.default_interface_font);
+			
 		return resources;
 	}
+	
 	public static void setLocale(Locale l) {
 		Locale[] locales = Locale.getAvailableLocales();
 		for (int k=0; k<locales.length; k++)
@@ -34,9 +52,59 @@ public class I18n {
 				break;
 			}
 	}
+	
 	public static Locale getLocale() {
 		if (locale == null)
-			locale = Locale.getDefault();
+		{
+			int language = PreferenceManager.default_language;
+			if (language == -1)
+				locale = Locale.getDefault();
+			else
+				locale = new Locale(languageCodes[language]);
+		}
 		return locale;
 	}
+	
+	public static String getDefaultDisplayLanguage()
+	{
+		return resources.getString(locale.getDisplayLanguage());
+	}
+	
+	public static String[] getSupportedLanguages()
+	{
+		String languages[] = new String[2];
+		languages[0] = resources.getString("English");
+		languages[1] = resources.getString("Chinese");
+		return languages;
+	}
+	
+	public static String[] getSupportedFonts(int lang)
+	{
+		SimplifiedLinkedList fonts = new SimplifiedLinkedList();
+		Font[] allfonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAllFonts();
+		int fontcount = 0;
+		String testStrings[] = {"Ok", "\u786e\u5b9a"};
+		
+		for (int j = 0; j < allfonts.length; j++)
+		{
+		    if (allfonts[j].canDisplayUpTo(testStrings[lang]) == -1)
+		    { 
+		        fonts.addSortedUnique(allfonts[j].getFamily());
+		    }
+		}
+		return fonts.toReverseStringArray();
+	}
+	
+	public static void setSystemFont(String font)
+	{
+		FontUIResource f = new FontUIResource(font, Font.PLAIN, 13); 
+	    java.util.Enumeration keys = UIManager.getDefaults().keys();
+	    
+	    while (keys.hasMoreElements()) {
+	      Object key = keys.nextElement();
+	      Object value = UIManager.get (key);
+	      if (value instanceof javax.swing.plaf.FontUIResource)
+	        UIManager.put (key, f);
+	      }		
+	}	
 }
