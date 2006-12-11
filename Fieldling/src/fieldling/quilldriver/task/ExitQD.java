a419e9165b6e82e74b795850ae8534c6f228c531
amontano <>
2006-12-11 20:53:51 +0000

    For some unexplained reason, in my development box (win xp, sp2) system.exit does not kill all of the java threads, leading to memory leaks after starting several instances of quilldriver. Now I am forcing the vm to halt if the normal system.exit does not kill all threads in 10 seconds.

--- Fieldling/src/fieldling/quilldriver/task/ExitQD.java
+++ Fieldling/src/fieldling/quilldriver/task/ExitQD.java
@@ -14,6 +14,19 @@ public class ExitQD extends ClosingTask {
             if (closeTranscript(qd) == -1)
                 return; //user cancelled close
         }
+		/* It is not shutting down under certain conditions in windows xp.
+		 * This makes sure VM does finish, with a grace period of 10 sec.
+		 */
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+		      public void run() {
+		        try {
+		          Thread.sleep(10000);
+		        } catch(InterruptedException ex) {}
+		        // halt will bail out without calling further shutdown hooks or
+		        // finalizers
+		        Runtime.getRuntime().halt(1);
+		      }
+		    });
         System.exit(0);
     }
 }
